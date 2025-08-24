7d6f5e7423224823e5e9900cce914b3783ee83fe
Darkentrol <oleksandrlubinskij@gmail.com>
2025-08-24 21:51:48 +0300

    Main menu v4

--- script/script_index.js
+++ script/script_index.js
@@ -1,6 +1,6 @@
 let title = document.getElementById("title_container");
 let text = title.textContent.trim();
-let colors = ["red", "green", "blue", "orange"];
+let colors = ["rgba(204, 46, 46, 1)", "rgba(47, 184, 47, 1)", "rgba(58, 67, 194, 1);", "rgba(224, 164, 53, 1)"];
 
 let newHTML = "";
 for(let i = 0; i < colors.length; i++) {
