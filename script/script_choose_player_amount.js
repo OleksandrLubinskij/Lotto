32fdd999b00ff95dde7d1f4bc22310cc163d0b6a
Darkentrol <oleksandrlubinskij@gmail.com>
2025-08-27 13:17:26 +0300

    Write gamemode info(Bot)

---
+++ script/script_choose_player_amount.js
@@ -0,0 +1,2 @@
+const data = JSON.parse(sessionStorage.getItem("settings"));
+console.log(data);
--- script/script_index.js
+++ script/script_index.js
@@ -1,5 +1,5 @@
-let title = document.getElementById("title_container");
-let text = title.textContent.trim();
+const title = document.getElementById("title_container");
+const text = title.textContent.trim();
 let colors = ["rgba(204, 46, 46, 1)", "rgba(47, 184, 47, 1)", "rgba(58, 67, 194, 1);", "rgba(224, 164, 53, 1)"];
 
 let newHTML = "";
@@ -7,3 +7,12 @@ for(let i = 0; i < colors.length; i++) {
     newHTML += `<span style="color:${colors[i]}; text-shadow: 1.5px 1.5px 10px ${colors[i]}">${text[i]}</span>`;
 }
 title.innerHTML = newHTML;
+
+
+const play_with_bot_btn = document.getElementsByClassName("gamemode_button")[0];
+play_with_bot_btn.addEventListener('click', function() {
+    sessionStorage.clear();
+    const settings = '{"gamemode": "Bot","players": 0,}';
+    sessionStorage.setItem("settings", JSON.stringify(settings));
+})
+
