e630794b8bb122acf625139cc976f10db644f621
Darkentrol <oleksandrlubinskij@gmail.com>
2025-08-28 19:38:58 +0300

    Fix bug with writing setting information

--- script/script_game.js
+++ script/script_game.js
@@ -2,6 +2,7 @@ const card = document.getElementById("card_template");
 const main_container = document.getElementById("main_container");
 const settings = JSON.parse(sessionStorage.getItem("settings"));
 const players_amount = settings.players;
+console.log(settings);
 for(let i = 0; i < players_amount; i++) {
     let clone = card.content.cloneNode(true);
     main_container.appendChild(clone);
