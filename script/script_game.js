14fefff9c2aecb56936d02c10b4ce832e4b5c47c
Darkentrol <oleksandrlubinskij@gmail.com>
2025-08-28 14:45:43 +0300

    Now in game may be 5 players(thanks to my cutie girl =^.^=)

--- script/script_game.js
+++ script/script_game.js
@@ -1,6 +1,8 @@
 const card = document.getElementById("card_template");
 const main_container = document.getElementById("main_container");
-for(let i = 0; i < 4; i++) {
+const settings = JSON.parse(sessionStorage.getItem("settings"));
+const players_amount = settings.players;
+for(let i = 0; i < players_amount; i++) {
     let clone = card.content.cloneNode(true);
     main_container.appendChild(clone);
 }
